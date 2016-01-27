7eaeb45bacc5ca341052309be328d8c98502c4b1
Ian Rees <ian@ianrees.net>
2016-01-27 14:47:32 -0800

    Additional specs

--- spec/gtfs/source_spec.rb
+++ spec/gtfs/source_spec.rb
@@ -5,7 +5,7 @@ describe GTFS::Source do
     File.expand_path(File.dirname(__FILE__) + '/../fixtures/valid_gtfs.zip')
   end
 
-  let(:source_missing_required_files) do 
+  let(:source_missing_required_files) do
     File.expand_path(File.dirname(__FILE__) + '/../fixtures/missing_files.zip')
   end
 
@@ -46,6 +46,67 @@ describe GTFS::Source do
   describe '#new(source)' do
     it 'should not allow a base GTFS::Source to be initialized' do
       lambda {GTFS::Source.new(valid_local_source)}.should raise_exception
+    end
+  end
+
+  describe '#file_present?' do
+    let(:source) {GTFS::Source.build(valid_local_source)}
+    it 'should return true if file is present' do
+      source.file_present?('agency.txt').should be true
+    end
+    it 'should return false if a file is not present' do
+      source.file_present?('missing.txt').should be false
+    end
+  end
+
+  describe '#required_files_present?' do
+    it 'should be true when all required files present' do
+      source = GTFS::Source.build(valid_local_source)
+      source.required_files_present?.should be true
+    end
+    it 'should be false when missing required files' do
+      source = GTFS::Source.build(source_missing_required_files)
+      source.required_files_present?.should be false
+    end
+    it 'should accept either calendar.txt or calendar_dates.txt' do
+      source = GTFS::Source.build(valid_local_source)
+      source.required_files_present?.should be true
+      # ... still valid without calendar.txt
+      File.unlink(source.send(:file_path, 'calendar.txt'))
+      source.required_files_present?.should be true
+      # ... but invalid when missing calendar.txt & calendar_dates.txt
+      File.unlink(source.send(:file_path, 'calendar_dates.txt'))
+      source.required_files_present?.should be false
+    end
+  end
+
+  describe '#load_shapes' do
+    let(:source) {GTFS::Source.build(valid_local_source)}
+    it 'should load shapes when file present' do
+      shapes = source.load_shapes
+      shapes.size.should > 0
+    end
+    it 'should return nil when file not present' do
+      File.unlink(source.send(:file_path, 'shapes.txt'))
+      shapes = source.load_shapes
+      shapes.should be nil
+    end
+    it '#shape_line should returns array of points' do
+      source.shape_line('63542').size.should be 9
+    end
+  end
+
+  describe '#load_service_periods' do
+    let(:source) {GTFS::Source.build(valid_local_source)}
+    it 'should return ServicePeriods' do
+      source.load_service_periods.size.should > 0
+    end
+    it 'should handle missing calendar/calendar_dates' do
+      File.unlink(source.send(:file_path, 'calendar.txt'))
+      source.load_service_periods.size.should > 0
+    end
+    it '#service_period returns ServicePeriod' do
+      source.service_period('1').class.should be GTFS::ServicePeriod
     end
   end
 

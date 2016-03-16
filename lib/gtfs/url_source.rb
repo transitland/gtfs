425de59a67d4b2f946c8a2cad2875e6b0038c127
Ian Rees <ian@ianrees.net>
2016-03-16 11:17:12 -0700

    Temporarily deprecate URLSource

--- lib/gtfs/url_source.rb
+++ lib/gtfs/url_source.rb
@@ -3,20 +3,19 @@ require 'uri'
 
 module GTFS
   class URLSource < Source
-
     def load_archive(source_url)
-      Dir.mktmpdir do |tmp|
-        file_name = File.join(tmp, "/gtfs_temp_#{Time.now.strftime('%Y%jT%H%M%S%z')}.zip")
-        uri = URI.parse(source_url)
-        response = Net::HTTP.get_response(uri)
-        open(file_name, 'wb') do |file|
-          file.write response.body
-        end
-        extract_to_cache(file_name)
-      end
+      source_url
+      # Dir.mktmpdir do |tmp|
+      #   file_name = File.join(tmp, "/gtfs_temp_#{Time.now.strftime('%Y%jT%H%M%S%z')}.zip")
+      #   uri = URI.parse(source_url)
+      #   response = Net::HTTP.get_response(uri)
+      #   open(file_name, 'wb') do |file|
+      #     file.write response.body
+      #   end
+      #   extract_to_cache(file_name)
+      # end
     rescue Exception => e
       raise InvalidSourceException.new(e.message)
     end
-
   end
 end

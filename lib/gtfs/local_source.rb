90cea4afc35699ea4bc9f3210dbba8a901fe1e4a
Ian Rees <ian@ianrees.net>
2016-03-22 14:29:28 -0700

    Extract nested zips

--- lib/gtfs/local_source.rb
+++ lib/gtfs/local_source.rb
@@ -1,5 +1,8 @@
 module GTFS
   class LocalSource < Source
+
+    attr_accessor :tmp_dir
+
     def load_archive(source_path)
       extract_to_cache(source_path)
     rescue Exception => e
@@ -25,42 +28,72 @@ module GTFS
       proc {FileUtils.rm_rf(directory)}
     end
 
-    def self.extract_nested(filename, path)
-
+    def self.extract_entry_zip(entry)
+      entry_dir, entry_name = File.split(entry.name)
+      Tempfile.open(entry_name) do |tmpfile|
+        tmpfile.binmode
+        tmpfile.write(entry.get_input_stream.read)
+        tmpfile.close
+        yield tmpfile.path
+        tmpfile.unlink
+      end
     end
 
-    def self.find_nested_gtfs(filename)
-      dirs = find_nested_archives(filename)
+    def self.extract_nested(filename, path, tmp_dir=nil)
+      path, _, fragment = path.partition('#')
+      path = "." if path == ""
+      tmp_dir ||= Dir.mktmpdir
+      # puts "path: #{path} fragment: #{fragment} tmp_dir: #{tmp_dir}"
+      Zip::File.open(filename) do |zip|
+        zip.entries.each do |entry|
+          entry_dir, entry_name = File.split(entry.name)
+          entry_ext = File.extname(entry_name)
+          # puts "entry_dir: #{entry_dir} entry_name: #{entry_name} entry_ext: #{entry_ext}"
+          if entry_dir == path && SOURCE_FILES.key?(entry_name)
+            # puts "\textract file: #{entry.name}"
+            entry.extract(File.join(tmp_dir, entry_name))
+          elsif entry.name == path && entry_ext == '.zip'
+            # puts "\textract zip: #{entry.name}"
+            extract_entry_zip(entry) do |tmppath|
+              extract_nested(tmppath, fragment, tmp_dir=tmp_dir)
+            end
+          end
+        end
+      end
+      tmp_dir
+    end
+
+    def self.find_gtfs_paths(filename)
+      # Find internal paths to valid GTFS data inside (possibly nested) Zips.
+      dirs = find_paths(filename)
         .select { |dir, files| required_files_present?(files) }
         .keys
     end
 
-    def self.find_nested_archives(filename, basepath=nil, limit=1000, count=0)
-      basepath ||= "/"
+    def self.find_paths(filename, basepath=nil, limit=1000, count=0)
+      # Recursively inspect a Zip archive, returning a directory index.
+      # Nested zip files will have the form:
+      #   nested.zip#inner_path
       dirs = {}
+      # Build paths manually, to avoid extra / at end
       Zip::File.open(filename) do |zip|
         zip.entries.each do |entry|
           raise Exception.new("Too many files") if count > limit
           count += 1
           entry_dir, entry_name = File.split(entry.name)
           entry_dir = "" if entry_dir == "."
-          entry_dir = File.join(basepath, entry_dir)
+          entry_dir = (basepath + entry_dir) if basepath
           entry_ext = File.extname(entry_name)
           dirs[entry_dir] ||= Set.new
           if entry_ext == '.zip'
-            tmpfile = Tempfile.open(entry_name)
-            Tempfile.open(entry_name) do |tmpfile|
-              tmpfile.binmode
-              tmpfile.write(entry.get_input_stream.read)
-              tmpfile.close
-              result = find_nested_archives(
-                tmpfile.path,
-                basepath=File.join(entry_dir, entry_name),
+            extract_entry_zip(entry) do |tmppath|
+              result = find_paths(
+                tmppath,
+                basepath=(basepath || "") + entry.name + '#',
                 limit=limit,
                 count=count
               )
               dirs = dirs.merge(result)
-              tmpfile.unlink
             end
           else
             dirs[entry_dir] << entry_name

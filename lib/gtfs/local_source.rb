39cc5a34c4d33009f87151df0f4e4a6e4feebc6f
Ian Rees <ian@ianrees.net>
2016-03-22 16:02:34 -0700

    Combine extract methods

--- lib/gtfs/local_source.rb
+++ lib/gtfs/local_source.rb
@@ -4,24 +4,12 @@ module GTFS
     attr_accessor :tmp_dir
 
     def load_archive(source_path)
-      extract_to_cache(source_path)
+      source_path, _, fragment = source_path.partition('#')
+      tmp_dir = self.class.extract_nested(source_path, fragment)
+      ObjectSpace.define_finalizer(self, self.class.finalize(tmp_dir))
+      tmp_dir
     rescue Exception => e
       raise InvalidSourceException.new(e.message)
-    end
-
-    def extract_to_cache(source_path)
-      # Temporary directory
-      @tmp_dir = Dir.mktmpdir
-      @path = @tmp_dir
-      ObjectSpace.define_finalizer(self, self.class.finalize(@tmp_dir))
-      # Extract
-      Zip::File.open(source_path) do |zip|
-        zip.entries.each do |entry|
-          next unless SOURCE_FILES.key?(entry.name)
-          entry.extract(file_path(entry.name))
-        end
-      end
-      @path
     end
 
     def self.finalize(directory)

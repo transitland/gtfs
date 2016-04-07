30e9a9ce4c0018bb47a8b77a15f9b43785de6080
Ian Rees <ian@ianrees.net>
2016-04-07 01:59:07 -0700

    URLSource#load_archive specs

--- spec/gtfs/url_source_spec.rb
+++ spec/gtfs/url_source_spec.rb
@@ -1,6 +1,26 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe GTFS::URLSource do
+
+  context '#load_archive' do
+    let(:source_path) {'http://dl.dropbox.com/u/416235/work/valid_gtfs.zip'}
+    it 'downloads' do
+      feed = nil
+      VCR.use_cassette('valid_gtfs_uri') do
+        feed = GTFS::URLSource.new(source_path)
+      end
+      feed.valid?.should be true
+    end
+
+    it 'sets @archive to downloaded temporary file' do
+      feed = nil
+      VCR.use_cassette('valid_gtfs_uri') do
+        feed = GTFS::URLSource.new(source_path)
+      end
+      File.exists?(feed.archive).should be true
+    end
+  end
+
   context 'with a URI to a valid source zip' do
     let(:source_path) {'http://dl.dropbox.com/u/416235/work/valid_gtfs.zip'}
     it 'should create a new source successfully' do

bb070df59dc9e068488f97badb2154bc99963daf
Ian Rees <ian@ianrees.net>
2016-04-08 17:27:28 -0700

    Additional exceptions

--- lib/gtfs/custom_exceptions.rb
+++ lib/gtfs/custom_exceptions.rb
@@ -1,4 +1,34 @@
 module GTFS
   class InvalidSourceException < Exception
+    # Base GTFS Exception class
+  end
+
+  class InvalidURLException < InvalidSourceException
+    # Invalid URL or hostname
+  end
+
+  class InvalidResponseException < InvalidSourceException
+    # Remote host returned error
+    attr_accessor :response_code
+    def initialize(msg, response_code=nil)
+      super(msg)
+      @response_code = response_code
+    end
+  end
+
+  class InvalidFileException < InvalidSourceException
+    # Invalid/missing local file
+  end
+
+  class InvalidZipException < InvalidSourceException
+    # Invalid Zip
+  end
+
+  class InvalidCSVException < InvalidSourceException
+    # Invalid CSV data
+  end
+
+  class InvalidEntityException < InvalidSourceException
+    # Invalid entity
   end
 end

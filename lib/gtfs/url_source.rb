5b0f23466bfd0741c028a975419677f74c64851d
Ian Rees <ian@ianrees.net>
2016-05-20 15:02:22 -0700

    tmp_dir fixes

--- lib/gtfs/url_source.rb
+++ lib/gtfs/url_source.rb
@@ -2,11 +2,10 @@ module GTFS
   class URLSource < ZipSource
     def load_archive(source)
       source_url, _, fragment = source.partition('#')
-      tmp_dir = Dir.mktmpdir
-      source_file = File.join(tmp_dir, "/gtfs_temp_#{Time.now.strftime('%Y%jT%H%M%S%z')}.zip")
+      tmp_dir = create_tmp_dir
+      source_file = File.join(tmp_dir, "gtfs_temp_#{Time.now.strftime('%Y%jT%H%M%S%z')}.zip")
       Fetch.download(source_url, source_file, progress: options[:progress_download])
-      self.class.extract_nested(source_file, fragment, tmp_dir: tmp_dir)
-      ObjectSpace.define_finalizer(self, self.class.finalize(tmp_dir))
+      self.class.extract_nested(source_file, fragment, tmp_dir)
       # Return unzipped path and source zip file
       return tmp_dir, source_file
     rescue SocketError => e

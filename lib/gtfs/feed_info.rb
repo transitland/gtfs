1144c5a1c87748fb379babb4dac3ab915c7ae367
Ian Rees <ian@ianrees.net>
2017-02-01 17:24:54 -0800

    Support feed_contact_email, feed_contact_url

--- lib/gtfs/feed_info.rb
+++ lib/gtfs/feed_info.rb
@@ -3,7 +3,7 @@ module GTFS
     include GTFS::Model
 
     has_required_attrs :feed_publisher_name, :feed_publisher_url, :feed_lang
-    has_optional_attrs :feed_start_date, :feed_end_date, :feed_version, :feed_id
+    has_optional_attrs :feed_start_date, :feed_end_date, :feed_version, :feed_id, :feed_contact_email, :feed_contact_url
     attr_accessor *attrs
 
     collection_name :feed_infos

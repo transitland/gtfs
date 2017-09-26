35fa2b39891a36c7892c95ee79ea2c641ea90cf8
Ian Rees <ian@ianrees.net>
2017-09-26 16:34:56 -0700

    URLSource: ssl_verify

--- lib/gtfs/url_source.rb
+++ lib/gtfs/url_source.rb
@@ -4,7 +4,7 @@ module GTFS
       source_url, _, fragment = source.partition('#')
       tmpdir = create_tmpdir
       source_file = File.join(tmpdir, "gtfs_temp_#{Time.now.strftime('%Y%jT%H%M%S%z')}.zip")
-      GTFS::Fetch.download(source_url, source_file, progress: options[:progress_download])
+      GTFS::Fetch.download(source_url, source_file, ssl_verify: options[:ssl_verify], progress: options[:progress_download])
       @source_filenames = self.class.extract_nested(source_file, fragment, tmpdir, options)
       # Return unzipped path and source zip file
       return tmpdir, source_file
